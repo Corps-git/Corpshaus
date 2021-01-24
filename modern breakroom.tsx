50ec02ef82acc6ea65acd80ca774ef5bdbb4f976
Corps-git <77861396+Corps-git@users.noreply.github.com>
2021-01-24 12:28:44 +0100

    Delete modern breakroom.tsx

--- modern breakroom.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="modern breakroom" tilewidth="32" tileheight="32" tilecount="576" columns="24">
- <image source="modern breakroom.png" width="768" height="768"/>
-</tileset>
