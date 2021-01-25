4db856c11ded1e61d5b64d1678645662e4e42356
Corps-git <77861396+Corps-git@users.noreply.github.com>
2021-01-25 11:45:19 +0100

    Create tilesets_deviant_milkian_1.tsx

---
+++ tilesets_deviant_milkian_1.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="neuer tileset" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="tilesets_deviant_milkian_1.png" width="512" height="512"/>
+ <tile id="201">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="202">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
